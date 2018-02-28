cfd734644e63edd6497a03c85fb1505769e5a3c6
SYSINF\tsdi202 <cocauw@gmail.com>
2018-02-28 11:50:04 +0100

    object on map get its position transformated to be displayed

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,8 @@ namespace prx
 {
 	
 	
-	enum Direction { Down, Left, Right, Up };
+	enum Direction { Right=0, Left=1, Down=2, Up=3 };
+	
 	namespace Sig
 	{
 		
@@ -30,14 +31,63 @@ namespace prx
 		
 	}
 	
-	struct Player
+	struct Point { int x, y; };
+	struct Object 
 	{
-		Player() {
-			this->texture.loadFromFile("resources/pacman_sprite.bmp");
-			sprite.setTexture(texture);
+		Object(const char *sprite_sheet_filepath) {
+			sf::Color color(255, 255, 255, 255);
+			if(!this->image.loadFromFile(sprite_sheet_filepath))
+				Sig::Quit.Emit();
+			this->image.createMaskFromColor(color, 0);
+			if(!this->texture.loadFromImage(this->image))
+				Sig::Quit.Emit();
+			this->sprite.setTexture(this->texture);
+			this->sprite.setPosition(0, 0);
+			this->setFrame(Direction::Right, 0);
 		}
+		void setFrame(enum Direction direction, int n) {
+			this->sprite.setTextureRect(sf::IntRect(n * 32, direction * 32,
+			                                        32, 32));
+		}
+		sf::Vector2f map_position;
+		sf::Image image;
 		sf::Texture texture;
 		sf::Sprite sprite;
+	};
+
+	struct ObjectCollection
+	{
+		ObjectCollection() { }
+		void add(Object* o) {
+			objects.push_back(o);
+		}
+		Object* get(int n) {
+			return this->objects[n];
+		}
+		std::vector<Object*> getAllObjects() {
+			return this->objects;
+		}
+		std::vector<Object*> objects;
+	};
+	
+	struct Map {
+		Map(ObjectCollection& oc) : objects(oc)
+		{ }
+		std::vector<Object*> getCell(sf::Vector2f position) {
+			std::vector<Object*> objects_at_position;
+			for(auto o: this->objects.getAllObjects())
+			{
+				if(position == o->map_position)
+					objects_at_position.push_back(o);
+			}
+			return objects_at_position;
+		}
+		ObjectCollection& objects;
+	};
+	
+	struct Player : Object
+	{
+		Player() : Object("resources/sprites/pacman.bmp") {}
 	};
 	
 	struct Keyboard
@@ -72,30 +122,40 @@ namespace prx
 	
 	struct Screen
 	{
-		Screen(sf::RenderWindow& w) : window(w) { }
+		Screen(sf::RenderWindow& w, Map& m) : window(w),
+		                                      map(m)
+		{ }
 		void draw() {
 			this->window.clear();
-			//this->window.draw();
+			for(auto& o: this->map.objects.getAllObjects())
+			{
+				o->sprite.setPosition(32 * o->map_position.x, 
+				                      32 * o->map_position.y);
+				this->window.draw(o->sprite);
+			}
 			this->window.display();
 		}
 		sf::RenderWindow& window;
+		Map& map;
 	};
 	
 	enum GameState { Init, Running, Error, Stopped };
-	
 	struct Game
 	{
 		Game(sf::ContextSettings context) : window(sf::VideoMode(640, 480),
 		                                           GAME_TITLE,
 		                                           sf::Style::Default,
 		                                           context),
-		                                    screen(window),
+		                                    map(objects),
+		                                    screen(window, map),
 		                                    spinner(window)
 		{
 			this->state = Init;
 			Sig::PlayerMove.Connect(this, &Game::move);
 			Sig::Quit.Connect(this, &Game::quit);
 			Sig::Tick.Connect(this, &Game::update);
+			this->player.map_position = sf::Vector2f(1, 1);
+			this->objects.add(&this->player);
 			this->window.setActive();
 		}
 		void launch() {
@@ -119,6 +179,8 @@ namespace prx
 		enum GameState state;
 		Keyboard keyboard;
 		sf::RenderWindow window;
+		ObjectCollection objects;
+		Map map;
 		Screen screen;
 		CPUSpinner spinner;
 		Player player;
